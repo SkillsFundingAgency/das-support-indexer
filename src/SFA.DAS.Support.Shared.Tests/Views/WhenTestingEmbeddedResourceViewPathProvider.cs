f1fd8a0fe89641280d4c241aa110d3e97beb3c28
Iain Stevenson <IainStevenson@live.co.uk>
2018-06-26 10:30:46 +0100

    Unit tests for View resolver modules

---
+++ src/SFA.DAS.Support.Shared.Tests/Views/WhenTestingEmbeddedResourceViewPathProvider.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.Hosting;
+using NUnit.Framework;
+using SFA.DAS.Support.Shared.Views;
+
+namespace SFA.DAS.Support.Shared.Tests.Views
+{
+    public class WhenTestingEmbeddedResourceViewPathProvider
+    {
+        private EmbeddedResourceViewPathProvider _unit;
+
+        [SetUp]
+        public void Setup()
+        {
+            _unit = new EmbeddedResourceViewPathProvider();
+        }
+
+        [Test]
+        public void ItShouldDetectAViewInTheSharedLibrary()
+        {
+            var path = "/Views/Shared/PanelLayout.cshtml";
+            var itemExists = _unit.FileExists(path);
+            Assert.IsTrue(itemExists);
+
+
+            var item = _unit.GetFile(path);
+            Assert.IsInstanceOf<VirtualFile>(item);
+            var file = item.Open();
+            Assert.IsNotNull(file);
+        }
+
+        [Test]
+        public void ItShouldResolveAViewInTheSharedLibrary()
+        {
+            var path = "/Views/Shared/PanelLayout.cshtml";
+            var item = _unit.GetFile(path);
+            Assert.IsInstanceOf<VirtualFile>(item);
+        }
+
+        [Test]
+        public void ItShouldRetrieveAViewFromTheSharedLibrary()
+        {
+            var path = "/Views/Shared/PanelLayout.cshtml";
+            var item = _unit.GetFile(path);
+            var file = item.Open();
+            Assert.IsNotNull(file);
+        }
+
+
+    }
+}
