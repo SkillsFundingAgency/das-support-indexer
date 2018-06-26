2daa51796513ed7ce9dde0c20310f81a6e53c7f7
Iain Stevenson <IainStevenson@live.co.uk>
2018-06-26 08:40:20 +0100

    Added View resolver for virtual paths and a readme for use.

---
+++ src/SFA.DAS.Support.Shared/Views/EmbeddedResourceFile.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Web.Hosting;
+
+namespace SFA.DAS.Support.Shared.Views
+{
+    public class EmbeddedResourceFile : VirtualFile
+    {
+        public EmbeddedResourceFile(string virtualPath) : base(virtualPath)
+        {
+        }
+
+        public static string GetResourceName(string virtualPath)
+        {
+            if (!virtualPath.Contains("/Views/")) return null;
+
+            var resourcename =
+                virtualPath
+                    .Substring(virtualPath.IndexOf("Views/"))
+                    // NB: Your assembly name here
+                    .Replace("Views/", "SFA.DAS.Support.Shared.Views.")
+                    .Replace("/", ".");
+
+            return resourcename;
+        }
+
+        public override Stream Open()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourcename = GetResourceName(VirtualPath);
+            // ReSharper disable once AssignNullToNotNullAttribute
+            return assembly.GetManifestResourceStream(resourcename);
+        }
+    }
+}
---
+++ src/SFA.DAS.Support.Shared/Views/EmbeddedResourceViewPathProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Web.Hosting;
+
+namespace SFA.DAS.Support.Shared.Views
+{
+    public class EmbeddedResourceViewPathProvider : VirtualPathProvider
+    {
+        private readonly Lazy<string[]> _resourceNames = new Lazy<string[]>(
+            () => Assembly.GetExecutingAssembly().GetManifestResourceNames(),
+            LazyThreadSafetyMode.ExecutionAndPublication);
+
+        private bool ResourceFileExists(string virtualPath)
+        {
+            var resourcename = EmbeddedResourceFile.GetResourceName(virtualPath);
+            var result = resourcename != null && _resourceNames.Value.Contains(resourcename);
+            return result;
+        }
+
+        public override bool FileExists(string virtualPath)
+        {
+            return base.FileExists(virtualPath) || ResourceFileExists(virtualPath);
+        }
+
+        public override VirtualFile GetFile(string virtualPath)
+        {
+            return !base.FileExists(virtualPath) ? new EmbeddedResourceFile(virtualPath) : base.GetFile(virtualPath);
+        }
+    }
+}
