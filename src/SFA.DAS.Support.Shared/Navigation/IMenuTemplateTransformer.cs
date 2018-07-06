70733c7925988c1b661ef76e1ad1508e3b1dbd0d
Iain Stevenson <IainStevenson@live.co.uk>
2018-07-06 12:34:17 +0100

    Added defautl common views and added MenuTransformer interface

---
+++ src/SFA.DAS.Support.Shared/Navigation/IMenuTemplateTransformer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SFA.DAS.Support.Shared.Navigation
+{
+    public interface IMenuTemplateTransformer
+    {
+        /// <summary>
+        ///     Transforms Menu templates into usable navigable Uri's
+        /// </summary>
+        List<MenuItem> TransformMenuTemplates(List<MenuItem> templateItems, Uri supportPortalUri,
+            Dictionary<string, string> identifiers);
+    }
+}
--- src/SFA.DAS.Support.Shared/Navigation/MenuTemplateTransformer.cs
+++ src/SFA.DAS.Support.Shared/Navigation/MenuTemplateTransformer.cs
@@ -5,25 +5,28 @@ using Newtonsoft.Json;
 
 namespace SFA.DAS.Support.Shared.Navigation
 {
-    /// <summary>
-    ///     Transforms Menu templates into usable navigable Uri's
-    /// </summary>
-    public class MenuTemplateTransformer
+    public class MenuTemplateTransformer : IMenuTemplateTransformer
     {
-        public List<MenuItem> TransformMenuTemplates(List<MenuItem> templateItems, Uri baseUrl,
+        public List<MenuItem> TransformMenuTemplates(List<MenuItem> templateItems, Uri supportPortalUri,
             Dictionary<string, string> identifiers)
         {
             var templateCopies =
                 JsonConvert.DeserializeObject<List<MenuItem>>(JsonConvert.SerializeObject(templateItems));
             var menuItems = templateCopies.Map(s => true, n => n.MenuItems).ToList();
             foreach (var menuItem in menuItems)
-            foreach (var identifier in identifiers)
-                menuItem.NavigateUrl =
-                    new Uri(baseUrl, menuItem.NavigateUrl)
-                        .OriginalString
-                        .Replace($"{{{identifier.Key}}}", identifier.Value);
+            {
+                foreach (var identifier in identifiers)
+                {
+                    menuItem.NavigateUrl =
+                        new Uri(supportPortalUri, menuItem.NavigateUrl)
+                            .OriginalString
+                            .Replace($"{{{identifier.Key}}}", identifier.Value);
+                }
+            }
             foreach (var menuItem in menuItems.ToList().Where(x => x.NavigateUrl.Contains("{")))
+            {
                 templateCopies.Remove(menuItem);
+            }
             return templateCopies;
         }
     }
